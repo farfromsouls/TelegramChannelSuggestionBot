35642a30cc3206b5994827b16231c0f35666d8de
LOLsite <farfromsouls@gmail.com>
2025-04-08 22:02:58 +0700

    ОНО ЗАРАБОТАЛО немного

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 from aiogram import Bot, Dispatcher, types
 from aiogram.filters.command import Command
 
+from data import *
+
 import asyncio
 import logging
 
@@ -20,12 +22,27 @@ async def handler(message: types.Message):
     id = message.from_user.id
     text = message.text
 
-    if get_lm() == "":
-        set_name()
-        bot.send_message("")
+    if check_id(id) == 0:
+        set_id(id)
 
-    if 
-
+    if get_lm(id) == None:
+        await set_brand(id, text)
+        await bot.send_message(id, "Напиши название чмо")
+        await set_lm(id, "Напиши название чмо")
+    elif get_lm(id) == "Напиши название чмо":
+        await set_name(id, text)
+        await bot.send_message(id, "Напиши описание чмо")
+        await set_lm(id, "Напиши описание чмо")
+    elif get_lm(id) == "Напиши описание чмо":
+        await set_ove(id, text)
+        await bot.send_message(id, "Напиши цену товара")
+        await set_lm(id, "Напиши цену товара")
+    elif get_lm(id) == "Напиши цену товара":
+        await set_price(id, text)
+        await bot.send_message(id, "Пришли фото хоточкэ :3 <3")
+        await set_lm(id, "Пришли фото хоточкэ :3 <3")
+    elif get_lm(id) == "Пришли фото хоточкэ :3 <3":
+        pass
 
 
 async def main():
